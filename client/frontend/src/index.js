4a65ef6abd1e00a5b056283d5e37e72b1c8e7db9
sJxlim <SAMLIMJX25.24@ichat.sp.edu.sg>
2025-03-06 16:36:36 +0800

    register design fixed

--- client/frontend/src/index.js
+++ client/frontend/src/index.js
@@ -3,13 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './core/index.css';
-import Choose from './pages/choose/choose';
+import Register from './pages/register/register';
 import reportWebVitals from './core/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Choose />
+    <Register />
   </React.StrictMode>
 );
 
